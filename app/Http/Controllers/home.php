e01edac9a55dfd01cd15a8f8591dcd64579ace0e
Ubuntu <ubuntu@ip-172-31-8-44.eu-west-1.compute.internal>
2019-10-15 09:38:16 +0000

    youtube issues

--- app/Http/Controllers/home.php
+++ app/Http/Controllers/home.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
 class home extends Controller
 {
     /**
@@ -19,44 +16,44 @@ class home extends Controller
         $request->session()->forget('v');
         $request->session()->forget('n');
 
-        $api = getenv('youtubeAPI');
-        $countryCode = getenv('countryCode');
-        $url = 'https://www.googleapis.com/youtube/v3/videos?chart=mostPopular&part=snippet&regionCode='.$countryCode.'&maxResults=4&key='.$api;
+//         $api = getenv('youtubeAPI');
+//         $countryCode = getenv('countryCode');
+//         $url = 'https://www.googleapis.com/youtube/v3/videos?chart=mostPopular&part=snippet&regionCode='.$countryCode.'&maxResults=4&key='.$api;
 
-        $content = file_get_contents($url);
-        $json = json_decode($content, true);
+//         $content = file_get_contents($url);
+//         $json = json_decode($content, true);
 
-        $n=0;
+//         $n=0;
 
-        foreach($json['items'] as $item) {
+//         foreach($json['items'] as $item) {
 
-            $link = new \stdClass;
+//             $link = new \stdClass;
 
-            $link -> vidId = $item['id'];
-            $link -> title = $item['snippet']['title'];
-            $link -> thumb =$item['snippet']['thumbnails']['high']['url'];
+//             $link -> vidId = $item['id'];
+//             $link -> title = $item['snippet']['title'];
+//             $link -> thumb =$item['snippet']['thumbnails']['high']['url'];
 
-            if ($n+1 == 10){ $link -> accesskey = '0'; }
-            elseif ($n+1 == 11){ $link -> accesskey = 'a'; }
-            elseif ($n+1 == 12){ $link -> accesskey = 'b'; }
-            else { $link -> accesskey = $n+1; }
+//             if ($n+1 == 10){ $link -> accesskey = '0'; }
+//             elseif ($n+1 == 11){ $link -> accesskey = 'a'; }
+//             elseif ($n+1 == 12){ $link -> accesskey = 'b'; }
+//             else { $link -> accesskey = $n+1; }
 
-            $links[] = $link;
+//             $links[] = $link;
 
-            //temp store array of vidId relative to search result position for next video functionality
-            // $request->session()->flash($item['id']['videoId'], $n);
-            $n++;
+//             //temp store array of vidId relative to search result position for next video functionality
+//             // $request->session()->flash($item['id']['videoId'], $n);
+//             $n++;
 
-        }
+//         }
 
-    	$data = [
-			'links' => $links,
-		];
+//     	$data = [
+        // 			'links' => $links,
+        // 		];
 
-    session(['v' => '']);
+        session(['v' => '']);
 
-    return view('home')->with($data);
+        return view('home');
+
+        // return view('home')->with($data);
     }
-
-
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,33 +12,12 @@
 <div class="row">
     <div class="large-6 large-centered columns text-center">
         <div>
-            <h4 class="popular">Most popular videos on YouTube</h4>
+            <h4 class="popular">Access: YouTube is having some major issues as Google have reduced our daily YouTube search quota from 3,000,000 to zero! I'm actively working with Google in trying to resolve this. As a result some days the site will work, and some days it won't. Bear with me! Mike </h4>
         </div>
     </div>
 </div>
 
-<div class="row">
-    <div class="medium-10 medium-centered columns">
-        <div class="row align-center text-center small-up-2 medium-up-4 large-up-4">
-            @foreach ($links as $link)
 
-                <div class="column">
-                    <div class="card focus">
-                       <div class="image">
-                            <a href="play/{{ $link -> vidId }}" accesskey="{{ $link -> accesskey }}">
-                                <img src="{{ $link -> thumb }}" alt="{{ $link -> title }}"/>
-                                <div class="content">
-                                    <p>{{ $link -> title }}</p>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-            @endforeach
-        </div>
-    </div>
-</div>
 
 
 @endsection
