cfdbd21fa4a24e8a20c8e071d0947fd21cf1407d
Ubuntu <ubuntu@ip-172-31-8-44.eu-west-1.compute.internal>
2020-01-16 14:36:09 +0000

    webcast

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
 <div class="row">
     <div class="large-6 large-centered columns text-center">
         <div>
-            <h4 class="popular">Access: YouTube is having some major issues as Google have reduced our daily YouTube search quota from 3,000,000 to zero! I'm actively working with Google in trying to resolve this. As a result some days the site will work, and some days it won't. Bear with me! Mike </h4>
+            <!--<h4 class="popular">Access: YouTube is having some major issues as Google have reduced our daily YouTube search quota from 3,000,000 to zero! I'm actively working with Google in trying to resolve this. As a result some days the site will work, and some days it won't. Bear with me! Mike </h4>-->
         </div>
     </div>
 </div>
