35bb5954f10a1ffad1a1e25c6782f946d12194d2
Ubuntu <ubuntu@ip-172-31-8-44.eu-west-1.compute.internal>
2020-04-24 15:37:10 +0000

    starting branding

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section('title', 'ACCESS: YouTube')
+@section('title', 'Access : YouTube')
 
 @section('content')
 
@@ -15,7 +15,17 @@
             <div class="row">
 
 </div>
-            <h4 class="popular">WE'RE BACK!</h4>
+
+</br></br></br>         
+            <h4>Access : YouTube is provided by:</h4>
+           <a tabindex="-1" href="https://accesstechnology.co.uk" target="_blank"><img tabindex="-1"  src="{{env('APP_URL')}}/img/accessTechnology.png" alt="access technology. working with people. empowering through technology"/></a>
+            <p class="sub">Access : technology is an assistive technology consultancy service specialising in supporting clients with brain injuries within the medico-legal sector.</p >
+    </div>
+    </br></br>
+    <div>
+        <p>Originally supported by:</p>
+        <a tabindex="-1" href="http://henshaws.ac.uk" target="_blank"><img style="width:200px" src="{{env('APP_URL')}}/img/henshaws.png" alt="henshaws college"></a> &  <a tabindex="-1" href="http://jisc.ac.uk" target="_blank"><img tabindex="-1" style="width:78px" src="{{env('APP_URL')}}/img/jisc.jpg" alt="jisc"></a>
+        
     </div>
 </div>
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>@yield('title')</title>
-        <meta name="description" content="An accessible interface for users of assistive technologies to play YouTube videos independently.">
+        <meta name="description" content="Access Youtube: An accessible interface for users of assistive technology to search for and play YouTube videos independently.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
