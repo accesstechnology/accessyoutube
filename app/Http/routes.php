04e619da03b57561c2af488f7c96d1ff328ca556
Ubuntu <ubuntu@ip-172-31-8-44.eu-west-1.compute.internal>
2020-04-24 14:26:10 +0000

    .

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,17 +11,17 @@
 |
 */
 
-Route::get('/youtube/', 'home@index');
+Route::get('/', 'home@index');
 
 // Route::get('clear', 'clear@index');
 
 Route::get('dbCheck', 'clear@create');
 
-Route::match(array('GET', 'POST'),'/youtube/{v}', [
+Route::match(array('GET', 'POST'),'/{v}', [
     'middleware' => ['rewrite','dbCheck'],
     'uses' => 'search@index'
 ]);
 
-Route::get('/youtube/play/next', 'play@next');
+Route::get('/play/next', 'play@next');
 
-Route::get('/youtube/play/{vidId}', 'play@index');
+Route::get('/play/{vidId}', 'play@index');
