bd8f00ef01df42dd76e8337b7835385909f548cd
Your Name <you@example.com>
2021-02-01 11:15:54 +0000

    .

--- app/helpers.php
+++ app/helpers.php
@@ -29,7 +29,7 @@ function rewrite ($v) {
 
 function search ($v) {
 
-    $url = "http://192.168.80.2:3000/api/search?q=".$v;
+    $url = "http://youtubescraper_node_1:3000/api/search?q=".$v;
         
     $return = file_get_contents($url);
     
