03c4db9bca332bae974933eb24f590305f169bf0
mike <mike.vscode.docker>
2021-02-04 00:26:43 +0000

    all present and correct

--- app/helpers.php
+++ app/helpers.php
@@ -29,7 +29,7 @@ function rewrite ($v) {
 
 function search ($v) {
 
-    $url = "http://accessyoutube.org.uk:3001/api/search?q=".$v;
+    $url = "http://youtube_scraper:3000/api/search?q=".$v;
         
     $return = file_get_contents($url);
     
