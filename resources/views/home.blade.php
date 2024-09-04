8b30a0b20bf5cc5fe0557971c7103e3fdcf15f9d
Mike <55278705+accesstechnology-mike@users.noreply.github.com>
2024-09-04 13:51:06 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,14 +9,14 @@
             <!-- Your existing content goes here -->
         </div>
         
-        <div class="additional-info" style="margin-top: 100px; padding: 20px; background-color: #f8f8f8; border-radius: 10px;">
+        <div class="additional-info" style="margin-top: 250px; padding: 20px; background-color: #f8f8f8; border-radius: 10px;">
             <h5 style="color: #333;">access: youtube is provided by:</h5>
             <a tabindex="-1" href="https://accesstechnology.co.uk" target="_blank">
                 <img tabindex="-1" width="150px" src="https://accesstechnology.co.uk/content/images/2024/03/Blue-Light-logo.png" alt="access: technology. working with people. empowering through technology" style="margin: 20px 0;">
             </a>
             
             <div style="margin-top: 20px; font-size: 0.9em; color: #555;">
-                <p><b></b>We now offer EHCP-funded AT assessments and intervention.<b></p>
+                <p style="font-weight: bold;">We now offer EHCP-funded AT assessments and intervention.</p>
                 <p>Contact us at <a href="mailto:domore@accesstechnology.co.uk" style="color: #007bff;">domore@accesstechnology.co.uk</a> to find out more.</p>
                 <p style="font-weight: bold;">#domorewithtechnology</p>
             </div>
