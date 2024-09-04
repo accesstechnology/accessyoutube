ddd514496d6c44f2a7a6687456535f3aa0b6d6a6
Mike <55278705+accesstechnology-mike@users.noreply.github.com>
2024-09-04 13:55:35 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
         <div class="additional-info" style="margin-top: 250px; padding: 20px; background-color: #f8f8f8; border-radius: 10px;">
             <p style="color: #333; margin-bottom:0px">access: youtube is provided by:</p>
             <a  tabindex="-1" href="https://accesstechnology.co.uk" target="_blank">
-                <img style="margin:0px" tabindex="-1" width="150px" src="https://accesstechnology.co.uk/content/images/2024/03/Blue-Light-logo.png" alt="access: technology. working with people. empowering through technology" style="margin: 20px 0;">
+                <img style="margin:10px" tabindex="-1" width="150px" src="https://accesstechnology.co.uk/content/images/2024/03/Blue-Light-logo.png" alt="access: technology. working with people. empowering through technology" style="margin: 20px 0;">
             </a>
             
             <div style="margin-top: 20px; font-size: 0.9em; color: #555;">
