587a3257aaebf0cb7d2ad9ecd4575d2bcdc474f5
Mike <55278705+accesstechnology-mike@users.noreply.github.com>
2024-09-04 13:44:46 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,17 +16,19 @@
 
 </div>
 
-<!--</br></br></br>         -->
-<!--            <h5>access: youtube is provided for free by:</h5>-->
-<!--           <a tabindex="-1" href="https://accesstechnology.co.uk" target="_blank"><img tabindex="-1" width="200px"   src="{{env('APP_URL')}}/img/accesstechnology.png" alt="access: technology. working with people. empowering through technology"/></a>-->
-<!--            <p class="sub">access: technology is an assistive technology consultancy service specialising in supporting clients with brain injuries within the medico-legal sector.</p >-->
-<!--    </div>-->
-<!--    </br></br>-->
-<!--    <div>-->
-<!--        <p>Originally supported by:</p>-->
-<!--        <a tabindex="-1" href="http://henshaws.ac.uk" target="_blank"><img style="width:200px" src="{{env('APP_URL')}}/img/henshaws.png" alt="henshaws college"></a> &  <a tabindex="-1" href="http://jisc.ac.uk" target="_blank"><img tabindex="-1" style="width:78px" src="{{env('APP_URL')}}/img/jisc.jpg" alt="jisc"></a>-->
+</br></br></br>   
+           <h5>access: youtube is provided for free by:</h5>
+     <a tabindex="-1" href="https://accesstechnology.co.uk" target="_blank"><img tabindex="-1" width="200px"   src="{{env('APP_URL')}}/img/accesstechnology.png" alt="access: technology. working with people. empowering through technology"/></a>
+        <p class="sub">access: technology is an assistive technology consultancy service specialising in supporting clients with brain injuries within the medico-legal sector.</p >-->
+ </div>
+   </br></br>
+ <div>
+     <p>We now offer EHCP-funded AT assessments and intervention.</p>
+<p>Contact us at <a href="mailto:domore@accesstechnology.co.uk">domore@accesstechnology.co.uk to find out more.</p>
+<p>#domorewithtechnology</p>
+    
         
-<!--    </div>-->
+    </div>
 </div>
 
 
